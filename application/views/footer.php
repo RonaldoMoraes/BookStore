b0edc18cac2f276b9a8a953ba9aa5f29653e859a
Ronaldo <ronaldo.moraes1990@gmail.com>
2015-11-13 17:15:03 -0200

    Footer fix(computador desligou no meio da alteraçao)

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,6 +2,16 @@
 
 ?>
 <!-- DIV CONTENT END -->
-</div>    
+</div>
+
+
+<!-- FOOTER START -->
+<footer class="" style="background-color: #f5f5f5; bottom: 0; height: 60px; position: absolute; width: 100%;">
+	<div class="container">
+		<p class="" style="margin: 20px 0px; color: #777;">Conteúdo do footer aqui.</p>
+	</div>
+</footer>
+<!-- FOOTER END -->
+
 </body>
 </html>
